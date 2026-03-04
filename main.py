d6237ad6a9bec682f0d5335fd2c7de3211359721
Dean <dqvillanueva@carc.pshs.edu.ph>
2026-03-04 22:28:01 +0800

    Added minor adjustments

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ try:
         # Loading the JSON file
         data = json.load(file)
 
+
 # Menu
     print("WELCOME TO BASKETBALL PLAYER STATISTICS RANKER")
-    print("INSTRUCTIONS:")
+    print("\nINSTRUCTIONS:")
     print("THIS PROGRAM HELPS YOU EXAMINE THE TOP 25 PLAYERS IN THE NBA RIGHT NOW.")
     print("THE PROGRAM WILL ASK YOU TO INPUT A RANK OF A PLAYER OUT OF 25.")
     print("THE PROGRAM WILL THEN PRINT THE STATISTICS OF THAT PLAYER INCLUDING HIS PERFORMANCE EFFICIENCY RATING AND HIS NAME.")
     print("THE PROGRAM WILL THEN RANK THE PLAYER ACCORDING TO THE PLAYER'S PERFORMANCE EFFICIENCY RATING.")
 
-    choice = int(input("Enter the rank of the player you want to examine in the top 25:"))
+    choice = int(input("\nEnter the rank of the player you want to examine in the top 25: "))
 
 except FileNotFoundError:
     print("Error: The file 'data.json' was not found.")
