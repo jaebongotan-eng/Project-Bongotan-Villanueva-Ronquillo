9abb38fcd9137b274ba1f7683bfa95fc74694e8f
Dean <dqvillanueva@carc.pshs.edu.ph>
2026-03-04 22:05:00 +0800

    Added comments and a player in the json file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
-try:
-    filename="statistics.json"
-    with open (filename,'r') as file:
-        data=json.load(file)
 
+try:
+    filename = "statistics.json"
+    with open(filename,'r') as file:
+        # Loading the JSON file
+        data = json.load(file)
+
+# Menu
     print("WELCOME TO BASKETBALL PLAYER STATISTICS RANKER")
     print("INSTRUCTIONS:")
     print("THIS PROGRAM HELPS YOU EXAMINE THE TOP 25 PLAYERS IN THE NBA RIGHT NOW.")
@@ -11,3 +14,9 @@ try:
     print("THE PROGRAM WILL THEN PRINT THE STATISTICS OF THAT PLAYER INCLUDING HIS PERFORMANCE EFFICIENCY RATING AND HIS NAME.")
     print("THE PROGRAM WILL THEN RANK THE PLAYER ACCORDING TO THE PLAYER'S PERFORMANCE EFFICIENCY RATING.")
 
+    choice = int(input("Enter the rank of the player you want to examine in the top 25:"))
+
+except FileNotFoundError:
+    print("Error: The file 'data.json' was not found.")
+except json.JSONDecodeError as e:
+    print(f"Failed to decode JSON: {e}")
