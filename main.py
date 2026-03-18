c0bfd7be252fd19990047bbabbe153f8ed5398db
keronquillo-sketch <keronquillo@carc.pshs.edu.ph>
2026-03-18 09:23:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ try:
         data = json.load(file)
 
 # Menu
-    print("                                                               WELCOME TO BASKETBALL PLAYER STATISTICS RANKER")
-    print("  ")
-    print("                                \nINSTRUCTIONS:")
-    print("                                                    THIS PROGRAM HELPS YOU EXAMINE THE TOP 25 PLAYERS IN THE NBA RIGHT NOW.")
-    print("                                                        THE PROGRAM WILL ASK YOU TO INPUT A RANK OF A PLAYER OUT OF 25.")
-    print("                             THE PROGRAM WILL THEN PRINT THE STATISTICS OF THAT PLAYER INCLUDING HIS PERFORMANCE EFFICIENCY RATING AND HIS NAME.")
-    print("                                        THE PROGRAM WILL THEN RANK THE PLAYER ACCORDING TO THE PLAYER'S PERFORMANCE EFFICIENCY RATING.")
+    print("                                                                   WELCOME TO BASKETBALL PLAYER STATISTICS RANKER")
+    print("                                    \nINSTRUCTIONS:")
+    print("                                                        THIS PROGRAM HELPS YOU EXAMINE THE TOP 25 PLAYERS IN THE NBA RIGHT NOW.")
+    print("                                                            THE PROGRAM WILL ASK YOU TO INPUT A RANK OF A PLAYER OUT OF 25.")
+    print("                                 THE PROGRAM WILL THEN PRINT THE STATISTICS OF THAT PLAYER INCLUDING HIS PERFORMANCE EFFICIENCY RATING AND HIS NAME.")
+    print("                                            THE PROGRAM WILL THEN RANK THE PLAYER ACCORDING TO THE PLAYER'S PERFORMANCE EFFICIENCY RATING.")
 
     choice = int(input("\nEnter the rank of the player you want to examine in the top 25: "))
 
