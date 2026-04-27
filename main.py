e1b55ce7ffce67c1ac210f7700689cc9763b3ddd
Dean <dqvillanueva@carc.pshs.edu.ph>
2026-04-27 17:31:32 +0800

    Finalization of code, Polished

--- main.py
+++ main.py
@@ -31,73 +31,73 @@ def player_per(data,choice):
 
 #Create a function that displays the top 25 players ranked according to Points.
 def player_pts(data):
-    sorted_data=sorted(key=lambda player: player["Pts"], reverse=True)#Sort ranks the points in ascending order but reverse puts it in descending.
+    sorted_data=sorted(data, key=lambda player: player["Pts"], reverse=True)#Sort ranks the points in ascending order but reverse puts it in descending.
     for rank, player in enumerate(data, start=1):#Enumerate function adds a counter for the ranking.
         print(f"{rank}. {player['Name']} = {player['Pts']:.2f} Points")
 
 #Create a function that displays the top 25 players ranked according to Field Goals Made.
 def player_fgm(data):
-    sorted_data=sorted(key=lambda player: player["FGM"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["FGM"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['FGM']:.2f} Field Goals")
 
 #Create a function that displays the top 25 players ranked according to Field Goals Attempted.
 def player_fga(data):
-    sorted_data=sorted(key=lambda player: player["FGA"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["FGA"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['FGA']:.2f} Field Goals")
 
 #Create a function that displays the top 25 players ranked according to Free Throws Attempted.
 def player_fta(data):
-    sorted_data=sorted(key=lambda player: player["FTA"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["FTA"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['FTA']:.2f} Free Throws")
 
 #Create a function that displays the top 25 players ranked according to Free Throws Made.
 def player_ftm(data):
-    sorted_data=sorted(key=lambda player: player["FTM"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["FTM"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['FTM']:.2f} Free Throws")
 
 #Create a function that displays the top 25 players ranked according to Rebounds.
 def player_reb(data):
-    sorted_data=sorted(key=lambda player: player["REB"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["REB"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['REB']:.2f} Rebounds")
 
 #Create a function that displays the top 25 players ranked according to Assists.
 def player_ast(data):
-    sorted_data=sorted(key=lambda player: player["AST"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["AST"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['AST']:.2f} Assists")
 
 #Create a function that displays the top 25 players ranked according to Steals.
 def player_stl(data):
-    sorted_data=sorted(key=lambda player: player["STL"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["STL"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['STL']:.2f} Steals")
 
 #Create a function that displays the top 25 players ranked according to Blocks.
 def player_blk(data):
-    sorted_data=sorted(key=lambda player: player["BLK"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["BLK"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['BLK']:.2f} Blocks")
 
 #Create a function that displays the top 25 players ranked according to Turnovers
 def player_tov(data):
-    sorted_data=sorted(key=lambda player: player["TOV"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["TOV"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['TOV']:.2f} Turnovers")
 
 #Create a function that displays the top 25 players ranked according to Minutes per game.
 def player_min(data):
-    sorted_data=sorted(key=lambda player: player["MIN"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["MIN"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['MIN']:.2f} Minutes")
 
 #Create a function that displays the top 25 players ranked according to Player Efficiency Rating.
 def player_per2(data):
-    sorted_data=sorted(key=lambda player: player["PER"], reverse=True)
+    sorted_data=sorted(data, key=lambda player: player["PER"], reverse=True)
     for rank, player in enumerate(data, start=1):
         print(f"{rank}. {player['Name']} = {player['PER']:.2f}")
 
@@ -147,32 +147,35 @@ try:
     with open(filename,'r') as file:
         # Loading the JSON file
         data = json.load(file)
-# Menu
+#Menu
         print("|------------------------------------------------------------------------------------------------------------------------|")
-        print("| WELCOME TO BASKETBALL PLAYER STATISTICS RANKER                                                                         |")
+        print("| WELCOME TO BASKETBALL PLAYER STATISTICS RANKER!                                                                         |")
         print("|------------------------------------------------------------------------------------------------------------------------|")
         print("| THIS PROGRAM HELPS YOU EXAMINE THE TOP 25 PLAYERS IN THE NBA RIGHT NOW.                                                |")
         print("| THE PROGRAM WILL ASK YOU TO INPUT A CHOICE.                                                                            |")
         print("| EXAMINE A SPECIFIC NBA PLAYER'S STATS, THE AVERAGE STATS OF THE TOP 25 PLAYERS, OR RANK THE PLAYERS BY A CHOSEN STAT.  |")
+        print("|------------------------------------------------------------------------------------------------------------------------|")
         print("| IF YOU CHOSE TO EXAMINE A SPECIFIC NBA PLAYER'S STATS, THE FOLLOWING WILL HAPPEN:                                      |")
+        print("|--------                                                                                                                |")
         print("|     THE PROGRAM WILL ASK YOU TO INPUT A RANK OF A PLAYER OUT OF 25.                                                    |")
         print("|     THE PROGRAM WILL THEN PRINT THE STATISTICS OF THAT PLAYER INCLUDING HIS PERFORMANCE EFFICIENCY RATING AND HIS NAME.|")
         print("|     THE PROGRAM WILL THEN RANK THE PLAYER ACCORDING TO THE PLAYER'S PERFORMANCE EFFICIENCY RATING.                     |")
+        print("|------------------------------------------------------------------------------------------------------------------------|")
         print("| IF YOU CHOOSE THE SECOND OPTION, THE PROGRAM WILL PRINT THE AVERAGE STATISTICS OF THE TOP 25 PLAYERS (AVERAGE PTS,ETC.)|")
+        print("|------------------------------------------------------------------------------------------------------------------------|")
         print("| IF YOU CHOOSE THE THIRD OPTION, YOU'LL BE ASKED TO SELECT A STAT FOR RANKING (PTS,REB,TOV,ETC...)                      |")
-        print("|     YOU THE PROGRAM WILL THEN DISPLAY THE NAMES OF PLAYERS RANKED IN ORDER ACCORDING TO THE STAT.                      |")
+        print("|-------                                                                                                                 |")
+        print("|     THE PROGRAM WILL THEN DISPLAY THE NAMES OF PLAYERS RANKED IN ORDER ACCORDING TO THE STAT.                          |")
         print("|------------------------------------------------------------------------------------------------------------------------|")
 
 #Ask the user for choices
-    print("")
-    print("Enter 1 if you want to examine a specific player")
+    print("\nEnter 1 if you want to examine a specific player")
     print("Enter 2 if you want to get the top 25 players in the NBA according to a specific stat")
     print("Enter 3 if you want to get the average stats of the top 25 players in the NBA")
-    print("")
 
     while True:
         try:
-            choice_one= int(input("Enter your choice (1-3): (type 0 to exit) "))
+            choice_one= int(input("\nEnter your choice (1-3): (type 0 to exit) "))
 
 #Add conditional function to identify the user's choice
             if choice_one == 0:
@@ -214,7 +217,6 @@ try:
                 print("Enter 11 if you want the players to be ranked by Minutes")
                 print("Enter 12 if you want the players to be ranked by Player Efficiency Rating (PER)")
                 print("="*100)
-                print("")
 
                 while True:
                     try:
